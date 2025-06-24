99719288e094648a04d4859cded72e23f564132d
SvelteRust <oddharald@myhren.ai>
2025-06-25 01:14:31 +0200

    crazy platform

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,56 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("Platform")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Platform".to_owned(),
+        window_width: 1980,
+        window_height: 1080,
+        fullscreen: true,
+        sample_count: 4,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     loop {
-        clear_background(RED);
+        clear_background(LIGHTGRAY);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        // Main platform (bottom)
+        draw_rectangle(
+            screen_width() / 2.0 - 600.0,
+            screen_height() - 300.0,
+            1200.0,
+            300.0,
+            DARKBLUE,
+        );
+
+        // Left platform
+        draw_rectangle(
+            screen_width() / 2.0 - 750.0,
+            screen_height() - 600.0,
+            360.0,
+            60.0,
+            BLUE,
+        );
+
+        // Center platform
+        draw_rectangle(
+            screen_width() / 2.0 - 180.0,
+            screen_height() - 750.0,
+            360.0,
+            60.0,
+            BLUE,
+        );
+
+        // Right platform
+        draw_rectangle(
+            screen_width() / 2.0 + 390.0,
+            screen_height() - 600.0,
+            360.0,
+            60.0,
+            BLUE,
+        );
 
         next_frame().await
     }
